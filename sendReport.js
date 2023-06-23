71e9c8f0162fc51946fa3b1830e753f253399110
Edward Mutahi <edward.ngugi@thejitu.com>
2023-06-23 11:30:24 +0300

    sending report

---
+++ sendReport.js
@@ -0,0 +1,27 @@
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+  service: 'hotmail',
+  auth: {
+    user: 'edward.ngugi@thejitu.com',
+    pass: 'Code1234.'
+  }
+});
+const mailOptions = {
+  from: 'edward.ngugi@thejitu.com',
+  to: 'edward.ngugi@thejitu.com',
+  subject: 'Email with Document',
+  text: 'Please find the document attached.',
+  attachments: [
+    {
+      path: '/Users/Admin/Desktop/Week8Report.docx'
+    }
+  ]
+};
+transporter.sendMail(mailOptions, (error, info) => {
+  if (error) {
+    console.log('Error occurred:', error);
+  } else {
+    console.log('Email sent:', info.response);
+  }
+});
